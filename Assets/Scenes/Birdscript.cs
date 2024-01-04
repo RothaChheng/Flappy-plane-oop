57e692b8af410ed258228834e58ad7c01b81ad3c
kimhorn20223 <155133888+kimhorn20223@users.noreply.github.com>
2024-01-04 10:23:14 +0700

    Score and Game over

--- Assets/Scenes/Birdscript.cs
+++ Assets/Scenes/Birdscript.cs
@@ -6,18 +6,38 @@ public class Birdscript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapstrength;
+
+    // Add 
+    public Logicscript logic;
+    public bool birdIsAlive = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<Logicscript>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Add birdIsAlive in to condition if
+    private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) == true ){
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
+        {
             myRigidbody.velocity = Vector2.up * flapstrength;
         }
-        
+    }
+
+    // Update is called once per frame 
+    /* void Update()
+     {
+         if(Input.GetKeyDown(KeyCode.Space) == true){
+             myRigidbody.velocity = Vector2.up * flapstrength;
+         }
+
+     }*/
+
+    // Add
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
---
+++ Assets/pipeMiddleScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipeMiddleScript : MonoBehaviour
+{
+    public Logicscript logic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<Logicscript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        logic.addScore();
+    }
+}
